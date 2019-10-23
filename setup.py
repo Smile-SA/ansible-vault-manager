e0629c3976e9f92ced7d3bd76d025ce936b17344
Guillaume GILL <guillaume.gill@smile.fr>
2019-10-23 12:13:22 +0200

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except IOError:
 
 setup(
     name="ansible-vault-manager",
-    version="0.2.0",
+    version="0.2.1",
     description="Python tool to manage vault-ids",
     license="MIT",
     author="Guillaume GILL",
